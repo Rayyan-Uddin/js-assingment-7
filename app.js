0bc6ec7305e3b80b60c8a1a66952ddc33f182c6b
Rayyan-Uddin <rayyanuddin007@gmail.com>
2023-05-24 03:43:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+// -----------------js-assingment (26 - 30)------------------
+
+// Question no 1:
+// var int = prompt("Enter any Positive Integer:")
+//  console.log(Number(int))
+//  console.log(Math.round(int))
+//  console.log(Math.floor(int))
+//  console.log(Math.ceil(int))
+
+
+// Question no 2:
+// var int = prompt("Enter any Negative floating point:")
+//  console.log(Number(int))
+//  console.log(Math.round(int))
+//  console.log(Math.floor(int))
+//  console.log(Math.ceil(int))
+
+
+// Question no 3
+// const num = prompt("Enter any Number:");
+// let absValue = Math.abs(+num)
+// console.log(absValue)
+
+
+// Question no 4
+// let diceVal = Math.random() *10;
+// console.log(parseInt(diceVal))
+
+
+// Question no 5
+// let x = Math.random()*2
+
+// let y = +Math.ceil(x)
+// // console.log(y)
+// if(y===1){
+//     console.log("1 Random coin value: Tails")
+// }
+// if(y===2){
+//     console.log("2 Random coin value: Heads")
+// }
+
+
+// // Question no 6
+// let ranNum = Math.random()*100;
+// console.log(Math.round(ranNum))
+
+
+// // Questoin no 7
+// let weight = prompt("Enter your Weight:")
+// console.log("The weight of user is "+parseFloat(weight)+" Kilograms")
+
+
+// Question no 8
+// let guessNum = Math.random() *10;
+// guessNum = Math.round(guessNum)
+// let userGuess = +prompt("Enter any Number Between 1 - 10:")
+// if(guessNum === userGuess){
+//     console.log("Congrats you are Right.")
+// }
+// else if(guessNum !== userGuess){
+//     console.log("Try Again.")
+// }
+// console.log(guessNum)
